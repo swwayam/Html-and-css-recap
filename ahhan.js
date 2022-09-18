85df8c8741fa8d0684cc472db215abfc73c123ba
swwayamm <codingswayam24@gmail.com>
2022-09-18 06:58:15 +0530

    This is for the hw

---
+++ ahhan.js
@@ -0,0 +1,31 @@
+//console.log("Hello World!")
+
+// var number = 50;
+// var name = "Rahul";
+
+// console.log(number);
+// console.log(name);
+
+// console.log(name +" has scored " + number);
+
+// // Task 1 
+// // name, age , profession, salary
+
+// var num1 = 10
+// var num2 = 20
+
+// console.log("The addition is ", num1 % num2);
+// console.log(Math.PI);
+
+// Task 2
+// Calculate the BMI create a var kg and h  var bmi = (BMI = kg/h) 
+
+// Task 3 
+// Make the formula as weight / height square
+
+// Task 4
+// Modify Your Existing Bmi calculator
+
+var kg = prompt("Enter Your Weight in Kg's");
+console.log(kg);
+
