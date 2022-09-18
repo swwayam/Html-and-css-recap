fb6014ba70ef208ea1ef09247ed5199d0543d92b
atimsina09 <ahaantimsina@gmail.com>
2022-09-17 21:18:00 -0500

    i completed the tasks

--- ahaan.js
+++ ahaan.js
@@ -11,6 +11,12 @@
 // // Task 1 
 // // name, age , profession, salary
 
+var age = 45;
+var name = "Benjaman";
+var profession = "McDonalds Worker";
+var salary = 10000;
+console.log(name+" is "+age+" years old, and he is a "+profession+" and makes "+salary+" dollars every year.")
+
 // var num1 = 10
 // var num2 = 20
 
@@ -20,12 +26,25 @@
 // Task 2
 // Calculate the BMI create a var kg and h  var bmi = (BMI = kg/h) 
 
+var kg = 50
+var h = 1.74
+var bmi = kg/h
+
+console.log(name+" has a bmi of "+bmi)
+
 // Task 3 
 // Make the formula as weight / height square
+
+var bmi2 = kg/(h*h)
+
+console.log(name+" has a bmi of "+bmi2)
+
 
 // Task 4
 // Modify Your Existing Bmi calculator
 
-var kg = prompt("Enter Your Weight in Kg's");
-console.log(kg);
+var kg = prompt("Enter your weight in kg's")
+var h = prompt("Enter your height in meters")
+var bmi = kg/(h*h)
+alert(bmi)
 
