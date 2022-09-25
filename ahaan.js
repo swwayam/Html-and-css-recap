298f712ca929d64ebe2f907dbcdb824948ed3d04
atimsina09 <ahaantimsina@gmail.com>
2022-09-24 21:26:39 -0500

    task 6 done

--- ahaan.js
+++ ahaan.js
@@ -11,11 +11,11 @@
 // // Task 1 
 // // name, age , profession, salary
 
-var age = 45;
-var name = "Benjaman";
-var profession = "McDonalds Worker";
-var salary = 10000;
-console.log(name+" is "+age+" years old, and he is a "+profession+" and makes "+salary+" dollars every year.")
+//var age = 45;
+//var name = "Benjaman";
+//var profession = "McDonalds Worker";
+//var salary = 10000;
+//console.log(name+" is "+age+" years old, and he is a "+profession+" and makes "+salary+" dollars every year.")
 
 // var num1 = 10
 // var num2 = 20
@@ -26,27 +26,27 @@ console.log(name+" is "+age+" years old, and he is a "+profession+" and makes "+
 // Task 2
 // Calculate the BMI create a var kg and h  var bmi = (BMI = kg/h) 
 
-var kg = 50
-var h = 1.74
-var bmi = kg/h
+//var kg = 50
+//var h = 1.74
+//var bmi = kg/h
 
-console.log(name+" has a bmi of "+bmi)
+//console.log(name+" has a bmi of "+bmi)
 
 // Task 3 
 // Make the formula as weight / height square
 
-var bmi2 = kg/(h*h)
+//var bmi2 = kg/(h*h)
 
-console.log(name+" has a bmi of "+bmi2)
+//console.log(name+" has a bmi of "+bmi2)
 
 
 // Task 4
 // Modify Your Existing Bmi calculator
 
-var kg = prompt("Enter your weight in kg's")
-var h = prompt("Enter your height in meters")
-var bmi = kg/(h*h)
-alert(bmi)
+//var kg = prompt("Enter your weight in kg's")
+//var h = prompt("Enter your height in meters")
+//var bmi = kg/(h*h)
+//alert(bmi)
 
 // Math Operations
 // var num1 = 20;
@@ -66,16 +66,40 @@ alert(bmi)
 
 // Task 5
 
-var taskName = "Apple Iphone 12"
-var cost = "900"
-var tax = "100"
-var totalCost;
+//var taskName = "Apple Iphone 12"
+//var cost = "900"
+//var tax = "100"
+//var totalCost;
 
 // Given are the above variables console log the total cost
 // select the i and 1 from the taskName and log them to the console
 // find out the reminder of cost / tax
 
-console.log(parseInt(cost) + parseInt(tax))
-console.log(taskName[6])
-console.log(tax[0])
-console.log(cost % tax)
+//console.log(parseInt(cost) + parseInt(tax))
+//console.log(taskName[6])
+//console.log(tax[0])
+//console.log(cost % tax)
+
+// Task 6 
+
+// Do the following
+// 1. Take Input from the user
+// 2. Convert the input to a string
+// 3. Make the string lower case and uppercase
+// 4. Find the length of the string
+
+var input = prompt("raghh")
+
+
+var methods;
+
+methods = input.toUpperCase();
+console.log(methods);
+
+methods = input.toLowerCase();
+console.log(methods);
+
+methods = input.length;
+console.log(methods);
+
+
