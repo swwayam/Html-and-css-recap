60eb3a1d05aa73ed6e8c88aa6ce17ec4dafb77f5
swwayamm <codingswayam24@gmail.com>
2022-11-12 07:43:05 +0530

    c o

--- index.js
+++ index.js
@@ -367,9 +367,7 @@
 // Create a object of your favourite anime character
 // it should have following - 
 // name, name of the show, two it should consisits of 3 functions of sleeping walking and talking and when the function is called it needs alerted. 
-<<<<<<< HEAD
-// array inside the object - you need to put in three things that you like.
-=======
+
 // array inside the object - you need to put in three things that you like.
 
 // Classes and Objects
@@ -466,6 +464,6 @@
 
 
 
->>>>>>> f33ee0243d2bd5e896244a4c6fc62312a52fd435
+
 
 
